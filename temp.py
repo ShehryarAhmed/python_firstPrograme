0d1074fdb41c25bc57f6f67d0a46a1a81e660a59
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-09 15:35:52 +0500

    rename file names

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ total_break = 3
 count_break = 0
 print("Programe now start "+time.ctime())
 while (count_break < total_break):
-    time.sleep(2)
+    time.sleep(5)
     webbrowser.open("www.google.com")
     count_break = count_break + 1
 
