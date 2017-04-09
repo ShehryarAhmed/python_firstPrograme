e6255b3a7d2fdcffce1022dbe252a329e4ec5dc1
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-09 21:03:01 +0500

    error

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,15 @@
 import os
 def rename_files():
-    file_list = os.listdir(r"C:\Users\android\Desktop\python\prank")
-    print(file_list)
+    #get all file name
+    file_list = os.listdir(r"C:\Users\android\Desktop\python\prank")    
+    #print(file_list)
 
+    save_path = os.getcwd()
+    #print("current dir "+save_path)
+    os.chdir(r"C:\Users\android\Desktop\python\prank")
+
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None,"0123456789"))
+        os.chdir(save_path)
+        
 rename_files()
